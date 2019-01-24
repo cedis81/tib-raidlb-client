91c0cd17b15d804a821f22c6cbd6f862b4471fec
cedis81 <ccchom11@gmail.com>
2019-01-24 18:02:59 -0500

    Update/Create flash messages
    * fix typo in the auth messages
    * created generic raid success and failure messages

--- src/raid/messages.js
+++ src/raid/messages.js
@@ -1,12 +1,10 @@
 export default {
+  createRaidSuccess: 'Raid successfully created.',
+  createRaidFailure: 'Raid was not created successfully. Please try again.',
+  getRaidSuccess: 'Raids successfully retrieved!',
+  getRaidFailure: 'Raids were not successfully retrieved!',
+  updateRaidSuccess: 'Raid successfully updated.',
+  updateRaidFailure: 'Raid was not updated successfully. Please try again.',
   deleteRaidSuccess: 'Raid successfully deleted.',
-  // signUpFailure: 'Registration failed. Email may be taked, or passwords don\'t'
-  //                 + ' match.',
-  // signInSuccess: 'Welcome!',
-  // signInFailure: 'Failed to sign in. Check your email and password and try' +
-  //                 ' again.',
-  // signOutSuccess: 'Come back soon!',
-  // changePasswordSuccess: 'Password changed successfully!',
-  // changePasswordFailure: 'Failed to change passwords. Check your old password' +
-  //                        ' and try again.'
+  deleteRaidFailure: 'Raid was not deleted successfully. Please try again.'
 }

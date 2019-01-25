a8a8ef2a142489e51d46c976b87cb07fcf997b1d
cedis81 <ccchom11@gmail.com>
2019-01-24 20:59:36 -0500

    Fix update failure message

--- src/raid/messages.js
+++ src/raid/messages.js
@@ -4,7 +4,7 @@ export default {
   getRaidSuccess: 'Raids successfully retrieved.',
   getRaidFailure: 'Raids were not successfully retrieved.',
   updateRaidSuccess: 'Raid successfully updated.',
-  updateRaidFailure: 'Raid was not updated successfully. Please try again and enter both fields.',
+  updateRaidFailure: 'Raid was not updated successfully. Please try again and enter both fields or on our own raid.',
   deleteRaidSuccess: 'Raid successfully deleted.',
   deleteRaidFailure: 'Raid was not deleted successfully. Please try on a raid you created.'
 }
